9d109aa9145b42f5d910e1391f79ac752e19a7bf
Santaev Pavel <santaevp@gmail.com>
2017-07-31 21:46:39 +0700

    extracted yandex api key to environment variable

--- app/src/main/java/ru/santaev/clipboardtranslator/api/YandexApi.java
+++ app/src/main/java/ru/santaev/clipboardtranslator/api/YandexApi.java
@@ -5,10 +5,11 @@ import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
+import ru.santaev.clipboardtranslator.BuildConfig;
 
 public interface YandexApi {
 
-    String API_KEY = "<---ApiKey--->";
+    String API_KEY = BuildConfig.YANDEX_TRANSLATE_API_KEY;
     String SERVER_URL = "https://translate.yandex.net/";
 
     @FormUrlEncoded
