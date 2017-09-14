5e791152bec32f99b8ef8b3c83803cdc3114a2b1
Santaev Pavel <santaevp@gmail.com>
2017-09-15 00:01:37 +0700

    added view model test

---
+++ app/src/androidTest/java/ru/santaev/clipboardtranslator/TranslateTest.java
@@ -0,0 +1,62 @@
+package ru.santaev.clipboardtranslator;
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule;
+import android.support.test.annotation.UiThreadTest;
+import android.support.test.rule.UiThreadTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+
+import io.reactivex.Single;
+import ru.santaev.clipboardtranslator.api.TranslateResponse;
+import ru.santaev.clipboardtranslator.model.IDataModel;
+import ru.santaev.clipboardtranslator.viewmodel.TranslateViewModel;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class TranslateTest {
+
+    @Rule
+    public InstantTaskExecutorRule instantExecutorRule = new InstantTaskExecutorRule();
+
+    @Rule
+    public UiThreadTestRule uiThreadTestRule = new UiThreadTestRule();
+
+    private static final String TEST_SRC = "Тест";
+
+    @UiThreadTest
+    @Test
+    public void test() throws Exception {
+        IDataModel dataModel = request -> {
+            TranslateResponse item = new TranslateResponse();
+            item.setLang("ru");
+            item.setCode(200);
+
+            ArrayList<String> text = new ArrayList<>();
+            if (request.originText.equals(TEST_SRC)) {
+                text.add("Test1");
+            } else {
+                text.add("Test2");
+            }
+
+            item.setText(text);
+
+            return Single.just(item);
+        };
+
+        TranslateViewModel viewModel = new TranslateViewModel(dataModel);
+        viewModel.onOriginTextChanged(TEST_SRC);
+
+        Thread.sleep(1000);
+
+        String value = LiveDataUtil.getValue(viewModel.getTranslatedText());
+        Thread.sleep(20000);
+        value = LiveDataUtil.getValue(viewModel.getTranslatedText());
+        assertEquals(value, "Test1");
+    }
+}
